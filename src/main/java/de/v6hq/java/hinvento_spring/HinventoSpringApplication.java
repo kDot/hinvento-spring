1b5f6bac3b6f59baf1a2ca1de633ed0fd88d6a19
kDot <k-Laus@gmx.net>
2024-11-22 17:13:19 +0100

    Initial

---
+++ src/main/java/de/v6hq/java/hinvento_spring/HinventoSpringApplication.java
@@ -0,0 +1,13 @@
+package de.v6hq.java.hinvento_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HinventoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HinventoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/v6hq/java/hinvento_spring/HinventoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package de.v6hq.java.hinvento_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HinventoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
